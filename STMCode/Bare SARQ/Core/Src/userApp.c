69bf864405a352b7c208962f86487bc2a8952b67
Nathan <G00413711@atu.ie>
2025-11-06 14:28:11 +0000

    added sample c code for serial comms

--- STMCode/Bare SARQ/Core/Src/userApp.c
+++ STMCode/Bare SARQ/Core/Src/userApp.c
@@ -16,7 +16,10 @@ int _write(int file, char *ptr, int len) {
 
 void userApp() {
 	printf("Starting my app\r\n\n");
+	char rx_buffer[50];
 	while (1) {
-
+		// Receive data from Raspberry Pi -non blocking
+		HAL_UART_Receive(&huart1, (uint8_t*)rx_buffer, sizeof(rx_buffer), HAL_MAX_DELAY);
+		printf("Received: %s\n", rx_buffer);
 	}
 }

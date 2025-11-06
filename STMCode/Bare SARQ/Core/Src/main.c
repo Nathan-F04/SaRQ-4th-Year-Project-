4654c3030ab24ba006ef3bd099c10081e7641bad
Nathan <G00413711@atu.ie>
2025-11-06 18:18:21 +0000

    usart3 semi working

--- STMCode/Bare SARQ/Core/Src/main.c
+++ STMCode/Bare SARQ/Core/Src/main.c
@@ -221,10 +221,6 @@ static void MX_TIM2_Init(void)
   sConfigOC.Pulse = 0;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
   if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
   {
     Error_Handler();
--- STMCode/Bare SARQ/Core/Src/stm32l4xx_hal_msp.c
+++ STMCode/Bare SARQ/Core/Src/stm32l4xx_hal_msp.c
@@ -157,9 +157,8 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     /**TIM2 GPIO Configuration
     PA1     ------> TIM2_CH2
     PA2     ------> TIM2_CH3
-    PA5     ------> TIM2_CH1
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_5;
+    GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;

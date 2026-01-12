eeb853eb0244e995d6a699a2fb435e873c41745b
Nathan Ferry <G00413711@atu.ie>
2026-01-12 15:13:02 +0000

    new method of ik, works according to desmos2 ik

--- Python/inverse_kin/inverse_kin.py
+++ Python/inverse_kin/inverse_kin.py
@@ -1,74 +1,23 @@
 """Module example for only inverse kinematics"""
-from math import acos,atan, sqrt, pi
+from math import acos,atan, sqrt, pi, atan2
 
-#Globals
 #Length upper and lower here
 LEN_U_L = 60
+a = 60
+b = 112
 LEN_L_L = 112
-#Y is X axis
-STARTING_Y = 38
-#Z is y axis 
-STARTING_Z = -52
-ANG_C = 9.5
+
 #Beginning of inverse kinematics calculations
-#Check if I need to change to degrees instead of radians
-def servo_calc(x_coord, y_coord, z_coord):
-    """Calls methods to get servo angles before sending them over serial"""
-    s1 = j1_calc(x_coord, y_coord+STARTING_Y)
-    s2 = j2_calc(x_coord, y_coord+STARTING_Y, z_coord+STARTING_Z)
-    s3 = j3_calc(x_coord, y_coord+STARTING_Y, z_coord+STARTING_Z)
+def servo_calc(x, y, z):
+    d = sqrt(x**2 + y**2)
+    c = sqrt(d**2 + z**2)
+    theta1 = atan2(y, x) * 180/pi
+    alpha = atan2(d, -z) * 180/pi
+    beta = acos((a**2 + c**2 - b**2)/(2*a*c)) * 180/pi
+    theta2 = alpha + beta
+    theta3 = 180 - acos((a**2 + b**2 - c**2)/(2*a*b)) * 180/pi
+    print(f"Theta 1 is: {theta1} Theta 2 is: {theta2} Theta 3 is: {theta3} ")
 
-    print(f"Hip: {s1}")
-    print(f"Shoulder: {s2}")
-    print(f"Leg: {s3}")
-    #for the test to assert completion- may edit later
-    return 0
-    #Pyserial will eventually be sent below
-
-def j1_calc(x_len, y_len):
-    """Calculates hip servo angle"""
-    if y_len == 0:
-        j1 = 0
-        return j1
-    else:
-        j1 = atan(x_len/y_len)
-        j1 = (j1*180)/pi
-        j1 = 90-j1
-        return j1
-
-def j2_calc(x_len,y_len,z_len):
-    """Calculates shoulder servo angle"""
-    hyp_top = hyp_top_calc(x_len, y_len)
-    hyp_side = hyp_side_calc(hyp_top, z_len)
-    b = acos((pow(hyp_side,2)+pow(LEN_U_L,2)-pow(LEN_L_L,2))/(2*hyp_side*LEN_U_L))
-    b = b *(180)/pi
-    a = atan(z_len/hyp_top)
-    a = a *(180)/pi
-
-    #Addition since A is negative - double check
-
-    j2 = b + a
-    j2 = 90-j2
-    #90 degree diff here
-    return j2
-
-def j3_calc(x_len, y_len, z_len):
-    """Calculates elbow servo angle"""
-    hyp_top = hyp_top_calc(x_len, y_len)
-    hyp_side = hyp_side_calc(hyp_top, z_len)
-    j3 = acos((pow(LEN_U_L,2)+pow(LEN_L_L,2)-pow(hyp_side,2))/(2*LEN_U_L*LEN_L_L))
-    j3 = (j3*180)/pi
-    #Double check this
-    j3 = j3 - 90 + ANG_C
-    return j3
-
-def hyp_top_calc(x_len, y_len):
-    """Calculates top hypotenuse"""
-    return sqrt((pow(x_len,2)+ pow(y_len,2)))
-
-def hyp_side_calc(hyp_top, z_len):
-    """Calculates side hypotenuse"""
-    return sqrt((pow(hyp_top,2)+ pow(z_len,2)))
 
 #X, Y Z is passed or hip, horizontal and vertical
-servo_calc(0,0, 0) 
+servo_calc(0,47, -46) 

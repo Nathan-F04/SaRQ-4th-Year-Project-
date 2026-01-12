3da693b42a660af86cb521e7409a51cb81cac0bb
Nathan Ferry <G00413711@atu.ie>
2026-01-12 19:50:54 +0000

    fromatted code to legible var names

--- Python/inverse_kin/inverse_kin.py
+++ Python/inverse_kin/inverse_kin.py
@@ -1,23 +1,27 @@
 """Module example for only inverse kinematics"""
-from math import acos,atan, sqrt, pi, atan2
+from math import acos, sqrt, pi, atan2
 
-#Length upper and lower here
+#Length upper and lower globals
 LEN_U_L = 60
-a = 60
-b = 112
 LEN_L_L = 112
 
-#Beginning of inverse kinematics calculations
-def servo_calc(x, y, z):
-    d = sqrt(x**2 + y**2)
-    c = sqrt(d**2 + z**2)
-    theta1 = atan2(y, x) * 180/pi
-    alpha = atan2(d, -z) * 180/pi
-    beta = acos((a**2 + c**2 - b**2)/(2*a*c)) * 180/pi
-    theta2 = alpha + beta
-    theta3 = 180 - acos((a**2 + b**2 - c**2)/(2*a*b)) * 180/pi
-    print(f"Theta 1 is: {theta1} Theta 2 is: {theta2} Theta 3 is: {theta3} ")
+#Inverse kinematics calculations
+def servo_calc(x_co_ord, y_co_ord, z_co_ord):
+    #Distance to tip
+    dist_d = sqrt(x_co_ord**2 + y_co_ord**2)
+
+    #Hypotenuse distance
+    hypotenuse = sqrt(dist_d**2 + z_co_ord**2)
+
+    #Angle calculations
+    hip_a = atan2(y_co_ord, x_co_ord) * 180/pi
+    alpha = atan2(dist_d, -z_co_ord) * 180/pi
+    beta = acos((LEN_U_L**2 + hypotenuse**2 - LEN_L_L**2)/(2*LEN_U_L*hypotenuse)) * 180/pi
+    shoulder_a = alpha + beta
+    elbow_a = 180 - acos((LEN_U_L**2 + LEN_L_L**2 - hypotenuse**2)/(2*LEN_U_L*LEN_L_L)) * 180/pi
+    
+    print(f"Theta 1 is: {hip_a} Theta 2 is: {shoulder_a} Theta 3 is: {elbow_a} ")
 
 
 #X, Y Z is passed or hip, horizontal and vertical
-servo_calc(0,47, -46) 
+servo_calc(0,124, -16) 
